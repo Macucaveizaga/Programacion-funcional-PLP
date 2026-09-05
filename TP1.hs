0787344ee9ad4b1a56df7fe8bd1cc78395822172
Andres <Andres.veizaga127@gmail.com>
2026-09-05 16:46:10 -0300

    Correccion 2,3

--- TP1.hs
+++ TP1.hs
@@ -1,80 +1,86 @@
 module TP1 where
 
 data Caja = Bombilla Bool | Nada
-              deriving Eq
-instance Show Caja where
-    show = showDeCaja
+  deriving (Eq)
 
-showDeCaja :: Caja -> String 
+instance Show Caja where
+  show = showDeCaja
+
+showDeCaja :: Caja -> String
 showDeCaja (Bombilla True) = "💡"
 showDeCaja (Bombilla False) = "⚪️"
 showDeCaja (Nada) = "🛑"
 
-data Circuito = Caja     Caja
-              | Serie    Circuito Circuito
-              | Paralelo Caja Circuito Circuito Caja
-                  deriving Eq
+data Circuito
+  = Caja Caja
+  | Serie Circuito Circuito
+  | Paralelo Caja Circuito Circuito Caja
+  deriving (Eq)
+
 instance Show Circuito where
-    show = showDeCircuito
+  show = showDeCircuito
 
 showDeCircuito :: Circuito -> String
 showDeCircuito (Caja caja) = showDeCaja caja
 showDeCircuito (Serie circuitoInicial circuitoFinal) =
   (showDeCircuito circuitoInicial) ++ "-" ++ (showDeCircuito circuitoFinal)
 showDeCircuito (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
-  (showDeCaja cajaEntrada) ++
-  "{" ++ (showDeCircuito circuitoIzquierdo) ++ "}" ++
-  "{" ++ (showDeCircuito circuitoDerecho) ++ "}" ++
-  (showDeCaja cajaSalida)
+  (showDeCaja cajaEntrada)
+    ++ "{"
+    ++ (showDeCircuito circuitoIzquierdo)
+    ++ "}"
+    ++ "{"
+    ++ (showDeCircuito circuitoDerecho)
+    ++ "}"
+    ++ (showDeCaja cajaSalida)
 
 showDeCircuitoConEstructura :: Circuito -> String
 showDeCircuitoConEstructura (Caja caja) = showDeCaja caja
-showDeCircuitoConEstructura (Serie circuitoInicial circuitoFinal) = "(" ++
-  (showDeCircuitoConEstructura circuitoInicial) ++
-    "-" ++
-  (showDeCircuitoConEstructura circuitoFinal) ++ ")"
+showDeCircuitoConEstructura (Serie circuitoInicial circuitoFinal) =
+  "("
+    ++ (showDeCircuitoConEstructura circuitoInicial)
+    ++ "-"
+    ++ (showDeCircuitoConEstructura circuitoFinal)
+    ++ ")"
 showDeCircuitoConEstructura (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
-  (showDeCaja cajaEntrada) ++
-  "{" ++ (showDeCircuitoConEstructura circuitoIzquierdo) ++ "}" ++
-  "{" ++ (showDeCircuitoConEstructura circuitoDerecho) ++ "}" ++
-  (showDeCaja cajaSalida)
+  (showDeCaja cajaEntrada)
+    ++ "{"
+    ++ (showDeCircuitoConEstructura circuitoIzquierdo)
+    ++ "}"
+    ++ "{"
+    ++ (showDeCircuitoConEstructura circuitoDerecho)
+    ++ "}"
+    ++ (showDeCaja cajaSalida)
 
-on  = Bombilla True
+on = Bombilla True
+
 off = Bombilla False
 
-cajaOn   = Caja on
-cajaOff  = Caja off
+cajaOn = Caja on
+
+cajaOff = Caja off
+
 cajaNada = Caja Nada
 
 -- 1: recCircuito
 
-recCircuito :: (Caja -> b) -> (b -> b -> Circuito -> Circuito -> b) -> (b -> b -> b -> b -> Caja -> Circuito -> Circuito ->Caja -> b )-> Circuito -> b
-recCircuito fCaja fSerie fParalelo circ = case circ of 
+recCircuito :: (Caja -> b) -> (b -> b -> Circuito -> Circuito -> b) -> (b -> b -> b -> b -> Caja -> Circuito -> Circuito -> Caja -> b) -> Circuito -> b
+recCircuito fCaja fSerie fParalelo circ = case circ of
   Caja c -> fCaja c
-  Serie a b -> fSerie (rec a) (rec b) a b 
+  Serie a b -> fSerie (rec a) (rec b) a b
   Paralelo a c1 c2 b -> fParalelo (fCaja a) (rec c1) (rec c2) (fCaja b) a c1 c2 b
-  where rec = recCircuito fCaja fSerie fParalelo 
-
+  where
+    rec = recCircuito fCaja fSerie fParalelo
 
 -- 2: foldCircuito
 
-foldCircuito :: (Caja -> b) -> (b-> b -> b) -> (b -> b -> b -> b -> b) -> Circuito -> b
-foldCircuito fCaja fSerie fParalelo circ = case circ of
-  Caja c -> fCaja c 
-  Serie a b -> recCircuito fCaja (\recx recy x y -> (fSerie x recx) (fSerie y recy)) a b 
-  Paralelo c1 a b c2 -> fParalelo (fCaja c1) (rec a) (rec b) (fCaja c2)
-  where rec = recCircuito fCaja fSerie fParalelo 
+foldCircuito :: (Caja -> b) -> (b -> b -> b) -> (b -> b -> b -> b -> b) -> Circuito -> b
+foldCircuito fCaja fSerie fParalelo = recCircuito fCaja (\w x y z -> fSerie w x) (\a b c d e f g h -> fParalelo a b c d)
 
-
-foldr1 :: (a -> b -> b) -> b -> [a] -> b
-foldr1 f z lista = recr z (\x xs rec -> f x rec) lista
-
-recr :: b -> (a -> [a] -> b -> b) -> [a] -> b
-recr z f [] = z
-recr z f (x : xs) = f x xs (recr z f xs)
 -- 3 invertido
 
-invertido = undefined -- TODO: COMPLETAR
+invertido :: Circuito -> Circuito
+invertido = recCircuito Caja (\x y c1 c2 -> Serie y x) (\rc1 rx ry rc2 c1 x y c2 -> Paralelo c2 ry rx c1)
 
 -- 4: hayCaminoIluminado
 
@@ -96,7 +102,7 @@ esCircuitoProlijo = undefined -- TODO: COMPLETAR
 
 circuitoEmprolijado = undefined -- TODO: COMPLETAR
 
--- 9: tienenLaMismaEstructura 
+-- 9: tienenLaMismaEstructura
 
 tienenLaMismaEstructura = undefined -- TODO: COMPLETAR
 
